83d1ce9b157875203f7c2a4d3cd97cacd6927924
razorshark81 <stevestan004@gmail.com>
2023-10-11 18:08:49 -0400

    path added

--- My project/Assets/Scripts/Path.cs
+++ My project/Assets/Scripts/Path.cs
@@ -7,7 +7,7 @@ public class Path : MonoBehaviour
 {
     public Color lineColor;
     private List<Transform> nodes = new List<Transform>();
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = lineColor;
         Transform[] pathTransforms = GetComponentsInChildren<Transform>();
@@ -22,15 +22,17 @@ public class Path : MonoBehaviour
         for (int i = 0; i < nodes.Count; i++)
         {
             Vector3 currentNode = nodes[i].position;
-            Vector3 previousNode;
+            Vector3 previousNode = Vector3.zero;
             if (i > 0)
             {
-                previousNode = nodes[i - 1].position;
+
+                previousNode = nodes[i - 1].position ;
             }
             else if (i == 0 && nodes.Count > 1)
             {
                 previousNode = nodes[nodes.Count - 1].position;
             }
+            Gizmos.DrawLine(previousNode,currentNode);
         }
     }
 }
